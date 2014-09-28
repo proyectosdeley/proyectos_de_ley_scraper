1d062ea867b18d5746c76cd846bc6fe5fa949889
aniversarioperu <aniversarioperu1@gmail.com>
2014-09-29 00:25:53 +0300

    fixing congresita name

--- pdl_scraper/pdl_scraper/pipelines.py
+++ pdl_scraper/pdl_scraper/pipelines.py
@@ -4,12 +4,14 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from datetime import datetime
+import re
 from models import db_connect
 from scrapy.exceptions import DropItem
 
 class PdlScraperPipeline(object):
     def process_item(self, item, spider):
         item['fecha_presentacion'] = self.fix_date(item['fecha_presentacion'])
+        item['congresistas'] = self.parse_names(item['congresistas'])
         return item
 
     def fix_date(self, string):
@@ -23,3 +25,16 @@ class PdlScraperPipeline(object):
         except ValueError:
             mydate = datetime.date(datetime.strptime(string, '%d/%m/%Y'))
         return mydate
+
+    def parse_names(self, string):
+        """
+        :param string: Person names separated by commas.
+        :return: String of person names separated by colons and family names
+                 separated from given names by commas.
+        """
+        names = ""
+        for i in string.split(","):
+            i = re.sub("\s{2}", ", ", i)
+            names += i + "; "
+        names = re.sub(";\s$", "", names)
+        return names
--- pdl_scraper/test_pipelines.py
+++ pdl_scraper/test_pipelines.py
@@ -9,9 +9,18 @@ from pdl_scraper.spiders.proyecto_spider import ProyectoSpider
 class TestPipeline(unittest.TestCase):
     def setUp(self):
         self.pipeline = PdlScraperPipeline()
-        self.item = dict(fecha_presentacion= u'10/10/2013')
+        self.item = dict(fecha_presentacion= u'10/10/2013',
+                         congresistas=u'Espinoza Cruz  Marisol,Abugattás '
+                                      u'Majluf  Daniel Fernando,Acha Roma'
+                                      u'ni  Walter,Apaza Condori  Emiliano,'
+                                      u'Nayap Kinin  Eduardo,Reynaga'
+                                      u'Soto  Jhon Arquimides,Valencia '
+                                      u'Quiroz  Jaime Ruben'
+        )
 
     def test_process_item(self):
         result_item = self.pipeline.process_item(self.item, ProyectoSpider)
         self.assertEqual(result_item['fecha_presentacion'],
                          datetime.date(2013, 10, 10))
+        self.assertEqual(result_item['congresistas'][0:33], u'Espinoza Cruz, '
+                                                      u'Marisol; Abugattás')
