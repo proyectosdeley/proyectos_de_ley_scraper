0fae4f06d14daa59cda0aae6260ee7e0a71c4ea0
aniversarioperu <aniversarioperu1@gmail.com>
2014-10-08 22:39:59 +0300

    when PDF file has may extra dots

--- pdl_scraper/test_pdfurl_spider.py
+++ pdl_scraper/test_pdfurl_spider.py
@@ -34,6 +34,13 @@ class TestPdfUrlSpider(unittest.TestCase):
         expected = 'http://www2.congreso.gob.pe/Sicr/TraDocEstProc/Contdoc01_2011.nsf/d99575da99ebfbe305256f2e006d1cf0/e58512d0bfb9118d052579bb0054c0e2/$FILE/PL00864080312.-.pdf'
         self.assertEqual(expected, result)
 
+        codigo = '00963'
+        filename = codigo + '.html'
+        response = fake_response_from_file(filename)
+        result = self.spider.find_pdfurl(codigo, response)
+        expected = 'http://www2.congreso.gob.pe/Sicr/TraDocEstProc/Contdoc01_2011.nsf/d99575da99ebfbe305256f2e006d1cf0/72dfe9f6ee7af28a052579d000043256/$FILE/PL00963280312....pdf'
+        self.assertEqual(expected, result)
+
 
 def fake_response_from_file(filename, url=None):
     """
