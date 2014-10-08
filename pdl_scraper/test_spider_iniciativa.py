58a07609e9e7d35c87fc56e598523ce3ab0e0c5b
aniversarioperu <aniversarioperu1@gmail.com>
2014-10-08 23:09:47 +0300

    all tests for spider iniciativa

--- pdl_scraper/test_spider_iniciativa.py
+++ pdl_scraper/test_spider_iniciativa.py
@@ -2,8 +2,7 @@
 import os
 import unittest
 
-from scrapy.http import TextResponse, Request
-
+from _test_utils import fake_response_from_file
 from pdl_scraper.spiders.iniciativas_spider import IniciativaSpider
 
 
@@ -16,29 +15,12 @@ class TestSpiderIniciativa(unittest.TestCase):
         expected = 'http://www2.congreso.gob.pe/Sicr/TraDocEstProc/CLProLey2011.nsf/Sicr/TraDocEstProc/CLProLey2011.nsf/PAporNumeroInverso/D36F68EDA5474A7605257CAE005690F0?opendocument'
         self.assertEqual(expected, result[0])
 
+    def test_parse(self):
+        codigo = '02764'
+        filename = codigo + '.html'
+        response = fake_response_from_file(filename)
+        result = self.spider.parse(response)
+        expected = u'00154'
+        self.assertEqual(expected, result.next()['iniciativas_agrupadas'][0:5])
 
-def fake_response_from_file(filename, url=None):
-    """
-    Create a Scrapy fake HTTP response from a HTML file
-    @param file_name: The relative filename from the responses directory,
-                      but absolute paths are also accepted.
-    @param url: The URL of the response.
-    returns: A scrapy HTTP response which can be used for unittesting.
 
-    taken from http://stackoverflow.com/a/12741030/3605870
-    """
-    if not url:
-        url = 'http://www.example.com'
-
-    request = Request(url=url)
-    if not filename[0] == '/':
-        responses_dir = os.path.dirname(os.path.realpath(__file__))
-        file_path = os.path.join(responses_dir, filename)
-    else:
-        file_path = filename
-
-    file_content = open(file_path, 'r').read()
-
-    response = TextResponse(url=url, request=request, body=file_content)
-    response._encoding = 'latin-1'
-    return response
