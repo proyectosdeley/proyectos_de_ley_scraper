9c6c6a002831738cc7c496dd009ed89598e0a015
aniversarioperu <aniversarioperu1@gmail.com>
2014-10-26 23:27:39 +0200

    test for 00253

--- pdl_scraper/test_pdfurl_spider.py
+++ pdl_scraper/test_pdfurl_spider.py
@@ -12,41 +12,31 @@ class TestPdfUrlSpider(unittest.TestCase):
         self.spider = PdfUrlSpider()
 
     def test_find_pdfurl(self):
-        codigo = '00001'
-        filename = codigo + '.html'
-        response = fake_response_from_file(filename)
-        result = self.spider.find_pdfurl(codigo, response)
-        expected = 'http://www2.congreso.gob.pe/Sicr/TraDocEstProc' \
-                   '/Contdoc01_2011.nsf/d99575da99ebfbe305256f2e006d1cf0/e8ad7d6747e75b8e052578df005a92ab/$FILE/00001.pdf'
-        self.assertEqual(expected, result)
+        codigos = (
+            '00001',
+            '03847',
+            '00864',
+            '00963',
+            '01367',
+            '00052',
+            '00253',
+        )
+        expected = (
+            'http://www2.congreso.gob.pe/Sicr/TraDocEstProc/Contdoc01_2011.nsf/d99575da99ebfbe305256f2e006d1cf0/e8ad7d6747e75b8e052578df005a92ab/$FILE/00001.pdf',
+            'http://www2.congreso.gob.pe/Sicr/TraDocEstProc/Contdoc02_2011_2.nsf/d99575da99ebfbe305256f2e006d1cf0/4465dd7d442d1a6d05257d65007d0796/$FILE/PL03847021014.pdf',
+            'http://www2.congreso.gob.pe/Sicr/TraDocEstProc/Contdoc01_2011.nsf/d99575da99ebfbe305256f2e006d1cf0/e58512d0bfb9118d052579bb0054c0e2/$FILE/PL00864080312.-.pdf',
+            'http://www2.congreso.gob.pe/Sicr/TraDocEstProc/Contdoc01_2011.nsf/d99575da99ebfbe305256f2e006d1cf0/72dfe9f6ee7af28a052579d000043256/$FILE/PL00963280312....pdf',
+            'http://www2.congreso.gob.pe/Sicr/TraDocEstProc/Contdoc01_2011.nsf/0/8e0331a84969f79305257a4b007a7b2b/$FILE/01367300712.pdf',
+            'http://www2.congreso.gob.pe/Sicr/TraDocEstProc/Contdoc01_2011.nsf/d99575da99ebfbe305256f2e006d1cf0/d18f1338ca3a643b052578f1007a96ba/$FILE/00052PL1882011.pdf',
+            'http://www2.congreso.gob.pe/Sicr/TraDocEstProc/Contdoc01_2011.nsf/d99575da99ebfbe305256f2e006d1cf0/db59e670c91ac96b05257913006ebf4b/$FILE/PL00253220911---.pdf',
+        )
+        for i in range(len(codigos)):
+            codigo = codigos[i]
+            filename = codigo + '.html'
+            response = fake_response_from_file(filename)
+            result = self.spider.find_pdfurl(codigo, response)
+            self.assertEqual(expected[i], result)
 
-        codigo = '03847'
-        filename = codigo + '.html'
-        response = fake_response_from_file(filename)
-        result = self.spider.find_pdfurl(codigo, response)
-        expected = 'http://www2.congreso.gob.pe/Sicr/TraDocEstProc/Contdoc02_2011_2.nsf/d99575da99ebfbe305256f2e006d1cf0/4465dd7d442d1a6d05257d65007d0796/$FILE/PL03847021014.pdf'
-        self.assertEqual(expected, result)
-
-        codigo = '00864'
-        filename = codigo + '.html'
-        response = fake_response_from_file(filename)
-        result = self.spider.find_pdfurl(codigo, response)
-        expected = 'http://www2.congreso.gob.pe/Sicr/TraDocEstProc/Contdoc01_2011.nsf/d99575da99ebfbe305256f2e006d1cf0/e58512d0bfb9118d052579bb0054c0e2/$FILE/PL00864080312.-.pdf'
-        self.assertEqual(expected, result)
-
-        codigo = '00963'
-        filename = codigo + '.html'
-        response = fake_response_from_file(filename)
-        result = self.spider.find_pdfurl(codigo, response)
-        expected = 'http://www2.congreso.gob.pe/Sicr/TraDocEstProc/Contdoc01_2011.nsf/d99575da99ebfbe305256f2e006d1cf0/72dfe9f6ee7af28a052579d000043256/$FILE/PL00963280312....pdf'
-        self.assertEqual(expected, result)
-
-        codigo = '01367'
-        filename = codigo + '.html'
-        response = fake_response_from_file(filename)
-        result = self.spider.find_pdfurl(codigo, response)
-        expected = 'http://www2.congreso.gob.pe/Sicr/TraDocEstProc/Contdoc01_2011.nsf/0/8e0331a84969f79305257a4b007a7b2b/$FILE/01367300712.pdf'
-        self.assertEqual(expected, result)
 
 def fake_response_from_file(filename, url=None):
     """
