711f9a8c12e7f83f8de0b694ebd2ba2b4a028aeb
aniversarioperu <aniversarioperu1@gmail.com>
2014-12-09 14:33:48 +0200

    main functions ready

--- rank_db_search.py
+++ rank_db_search.py
@@ -9,46 +9,44 @@ db = dataset.connect("postgresql://postgres:mysqlboriska@localhost:5432/pdl")
 
 items = [
     {
-        'fecha_publicacion': '26/06/2014',
-        'texto': 'Asistencia y votación - Primera votación',
-        'expediente_url': 'http://www2.congreso.gob.pe/sicr/tradocestproc/Expvirt_2011.nsf/visbusqptramdoc/02764?opendocument',
+        'fecha': '25/06/2012',
+        'evento': '2Asistencia y votación - Primera votación',
+        'expediente_url': 'http://www2.congreso.gob.pe/sicr/tradocestproc/Expvirt_2011.nsf/visbusqptramdoc/01180?opendocument',
         'pdf_url': 'http://some_pdf_link.pdf1',
     },
     {
-        'fecha_publicacion': '26/06/2014',
-        'texto': 'Asistencia y votación - Segunda votación',
-        'expediente_url': 'http://www2.congreso.gob.pe/sicr/tradocestproc/Expvirt_2011.nsf/visbusqptramdoc/02764?opendocument',
-        'pdf_link': 'http://some_pdf_link.pdf2',
+        'fecha': '25/06/2012',
+        'evento': '2Asistencia y votación - Segunda votación',
+        'expediente_url': 'http://www2.congreso.gob.pe/sicr/tradocestproc/Expvirt_2011.nsf/visbusqptramdoc/01180?opendocument',
+        'pdf_url': 'http://some_pdf_link.pdf2',
     },
     {
-        'fecha_publicacion': '27/06/2014',
-        'texto': 'Some other event',
-        'expediente_url': 'http://www2.congreso.gob.pe/sicr/tradocestproc/Expvirt_2011.nsf/visbusqptramdoc/02764?opendocument',
-        'pdf_link': 'http://some_pdf_link.pdf3',
+        'fecha': '27/06/2014',
+        'evento': '1Some other event',
+        'expediente_url': 'http://www2.congreso.gob.pe/sicr/tradocestproc/Expvirt_2011.nsf/visbusqptramdoc/01180?opendocument',
+        'pdf_url': 'http://some_pdf_link.pdf3',
     },
 ]
 
 
-def get_fechas(items):
+def delete_from_db_if_match(item, proyecto_id):
     """
-    :param items:
-    :return: List of fechas in all our items.
+    If found in database delete. We can replace it soon afterwards.
+    :param item:
+    :param proyecto_id:
+    :return:
     """
-    fechas = []
-    append = fechas.append
-    for item in items:
-        if item['fecha_publicacion'] not in fechas:
-            append(item['fecha_publicacion'])
-
-    return fechas
+    table = db['pdl_seguimientos']
+    table.delete(proyecto_id=proyecto_id, fecha=item['fecha'])
 
 
-def update_insert_by_fecha(fecha, items):
-    items_to_process = []
-    append = items_to_process.append
-    for item in items:
-        if item['fecha_publicacion'] == fecha:
-            append(item)
+def get_proyecto_id_from_expediente_url(item):
+    table = db['pdl_proyecto']
+    res = table.find_one(expediente=item['expediente_url'])
+    if res is not None:
+        return res['id']
+    else:
+        return None
 
 
 def update_insert_items(items):
@@ -59,39 +57,19 @@ def update_insert_items(items):
     :param items:
     :return:
     """
-    fechas = get_fechas(items)
 
-    for fecha in fechas:
-        update_insert_by_fecha(fecha, items)
-    """
-    table = db['pdl_proyecto']
-    res = table.find_one(expediente=expediente_url)
-    if res is not None:
-        # Need to replace to 1ra and 2da
-        print(texto)
-        pattern = re.compile("PRIMERA", re.I)
-        texto = pattern.sub("1ra", texto)
+    for item in items:
+        tmp_fecha = datetime.datetime.strptime(item['fecha'], '%d/%m/%Y')
+        item['fecha'] = datetime.datetime.strftime(tmp_fecha, '%Y-%m-%d')
+        proyecto_id = get_proyecto_id_from_expediente_url(item)
+        item['proyecto_id'] = proyecto_id
+        delete_from_db_if_match(item, proyecto_id)
 
-        pattern = re.compile("SEGUNDA", re.I)
-        texto = pattern.sub("2da", texto).decode("utf-8")
-
-        fecha_obj = datetime.datetime.strptime(fecha_publicacion, '%d/%m/%Y')
-        fecha_iso = datetime.datetime.strftime(fecha_obj, '%Y-%m-%d')
-        proyecto_id = res['id']
-        # Query events in seguimientos with our date and proyecto_id
-        res_events = db.query("select * from pdl_seguimientos where proyecto_id=%s and "
-                              "fecha='%s'"
-                              % (str(proyecto_id), fecha_iso))
-        ratio = 0
-        best_match = {}
-        for i in res_events:
-            s = difflib.SequenceMatcher(lambda x: x == " ", texto, i['evento'])
-            if s.real_quick_ratio() > ratio:
-                ratio = s.real_quick_ratio()
-                best_match = i
-        print(best_match)
-        print(ratio)
-    """
+    for item in items:
+        table = db['pdl_seguimientos']
+        del item['expediente_url']
+        table.insert(item)
+        print item
 
 
 update_insert_items(items)
