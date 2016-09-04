74843d163c24bc6d5334dea43c9f93ae9ede82a8
carlosp420 <mycalesis@gmail.com>
2016-09-03 20:16:49 -0500

    small fixes to spiders

--- pdl_scraper/pdl_scraper/spiders/pdfurl_spider.py
+++ pdl_scraper/pdl_scraper/spiders/pdfurl_spider.py
@@ -35,7 +35,7 @@ class PdfUrlSpider(scrapy.Spider):
                 "\$FILE\/" + str(codigo) + "\.pdf$",
                 "\$FILE\/.*" + str(codigo) + "(PL)*[0-9]+\.*-*,?\.*pdf$",
                 "\$FILE\/.+" + str(codigo) + "[0-9]+\.PDF$",
-                "\/PL" + str(codigo) + "[0-9]+-?\.pdf",
+                "\/PL" + str(codigo) + "[0-9]+-?\.+pdf",
             ]
             for pattern in patterns:
                 pattern = re.compile(pattern)
